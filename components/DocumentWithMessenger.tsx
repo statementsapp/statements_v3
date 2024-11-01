c5e108e814b235fc4147a864c518e263d6f9a198
ok <statementsapp@gmail.com>
2024-11-01 13:04:29 +0700

    margin top eh

--- components/DocumentWithMessenger.tsx
+++ components/DocumentWithMessenger.tsx
@@ -184,9 +184,9 @@ export default function DocumentWithMessenger({
   }, []);
 
   return (
-    <div className="flex justify-center h-screen bg-black text-white overflow-hidden">
+    <div className="flex justify-center h-screen bg-black text-white overflow-hidden" style="padding-top: 50px">
       <div className="flex w-full max-w-7xl mx-auto">
-        <div className="w-2/3 p-6 bg-black border-r border-gray-800 overflow-hidden" id="document-container" style="padding-top: 50px">
+        <div className="w-2/3 p-6 bg-black border-r border-gray-800 overflow-hidden" id="document-container" >
           <InteractiveDocument
             ref={documentRef}
             onNewContent={handleNewContent}
